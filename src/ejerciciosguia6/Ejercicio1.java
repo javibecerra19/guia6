c2078426f851028ad80bbd2661da29fbf291c4d2
“Javier <javibecerra19@gmail.com>
2022-08-23 19:51:37 -0300

    Ejercicio 1 Guia 6

---
+++ src/ejerciciosguia6/Ejercicio1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosguia6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Javier
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int num1; int num2; 
+       
+ //int suma;
+        /**Escribir un programa que pida dos números enteros por teclado y calcule la suma de los
+dos. El programa deberá después mostrar el resultado de la suma
+*/
+
+        
+Scanner leer = new Scanner(System.in);
+
+
+System.out.println("Ingresa un numero:");
+num1 = leer.nextInt();
+System.out.println("Ingresa otro numero:");
+num2 = leer.nextInt();
+
+
+//suma = num1 + num2;
+       
+ System.out.println("El resultado es " + (num1 +num2) + " ");
+
+    
+    }
+}
